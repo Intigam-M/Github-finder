6db288b6392d73ac2f8316d260b60126bb0f0a10
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-02-09 12:23:11 +0400

    UsersReducer created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import Navbar from './components/Navbar'
 import UserList from './components/UserList';
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import Search from './components/Search';
 import Alert from './components/Alert';
+import UsersReducer from './reducers/usersReducer';
 
 
 const App = ()=>{
 
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(false)
+    // const [users, setUsers] = useState([])
+    // const [loading, setLoading] = useState(false)
+    const initialState = {
+        users: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(UsersReducer, initialState)
     const [error, setError] = useState(null)
 
 
     const searchUsers = (text) => {
-        setLoading(true);
+        setLoading();
         fetch(`https://api.github.com/search/users?q=${text}`)
         .then(res => res.json())
         .then(data => 
                 {
-                    setUsers(data.items)
-                    setLoading(false);
+                    // setUsers(data.items)
+                    dispatch({type: "SERACH_USERS", users: data.items})
+                    // setLoading(false);
                 }
             )
     }
 
+
+    const setLoading = () =>{
+        dispatch({ type:"SET_LOADING" })
+    }
+
     const clearResult = () => {
-        setUsers([])
+        dispatch({ type:"CLEAR_USERS"})
     }
 
     const displayAlert = (msg, type) => {
@@ -42,12 +55,12 @@ const App = ()=>{
         <Search 
             searchUsers={searchUsers} 
             clear={clearResult} 
-            clearButton={users.length > 0 ? true : false} 
+            clearButton={state.users.length > 0 ? true : false} 
             displayAlert={displayAlert} />
         <Alert error={error}/> 
 
         <div className="container mt-3">
-            <UserList users={users} key={users.id} loading={loading} />
+            <UserList users={state.users} key={state.users.id} loading={state.loading} />
         </div>
     </div>
     );
